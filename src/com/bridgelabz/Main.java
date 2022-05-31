021f2ec5f451b220c6a5baa1138e0f369eac52d1
Surendra234 <surendramahawar873@gmail.com>
2022-05-31 18:40:57 +0530

    Employee Wage Computaion

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("\nWelcome to Employee Wage Computation Program !\n");
-        EmployeeWage.computeEmpWage();
+        EmployeeWage.computeEmpWage("Maruti Suzuki",30,20,90);
+        EmployeeWage.computeEmpWage("Mahindra Tech",40,25,120);
+        EmployeeWage.computeEmpWage("Toyota",50,18,90);
     }
 }
